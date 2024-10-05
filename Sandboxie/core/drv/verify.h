987c166119a53b54aed24efb820835652e388cba
githubusername2024 <154524455+githubusername2024@users.noreply.github.com>
2024-10-05 10:00:11 +0200

    Update verify.h

--- Sandboxie/core/drv/verify.h
+++ Sandboxie/core/drv/verify.h
@@ -1,31 +1,14 @@
-/*
- * Copyright (C) 2021-2024 David Xanatos, xanasoft.com
- *
- * Process Hacker is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Process Hacker is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Process Hacker.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #define SOFTWARE_NAME L"Sandboxie-Plus"
 
 typedef union _SCertInfo {
     unsigned long long State;
     struct {
         unsigned long
-            active      : 1,    // certificate is active
-            expired     : 1,    // certificate is expired but may be active
-            outdated    : 1,    // certificate is expired, not anymore valid for the current build
-            unused_1    : 2,    // DEPRECATED
-            grace_period: 1,    // the certificate is expired and or outdated but we keep it valid for 1 extra month to allof wor a seamless renewal
+            active      : 1,      // certificate is active
+            expired     : 1,      // certificate is expired but may be active
+            outdated    : 1,      // certificate is expired, not anymore valid for the current build
+            unused_1    : 2,      // DEPRECATED
+            grace_period: 1,     // the certificate is expired and or outdated but we keep it valid for 1 extra month to allow for a seamless renewal
             reservd_2   : 2,
 
             type        : 5,
@@ -33,35 +16,25 @@ typedef union _SCertInfo {
 
             reservd_3   : 8,
 
-            reservd_4   : 4,    // More features
-            opt_desk    : 1,    // Isolated Sandboxie Desktops:             "UseSandboxDesktop"
-            opt_net     : 1,    // Advanced Network features:               "NetworkDnsFilter", "NetworkUseProxy".
-            opt_enc     : 1,    // Box Encryption and Box Protection:       "ConfidentialBox", "UseFileImage", "EnableEFS".
-            opt_sec     : 1;    // Various security enhanced box types:   "UseSecurityMode", "SysCallLockDown", "RestrictDevices", "UseRuleSpecificity", "UsePrivacyMode", "ProtectHostImages",
-                                // as well as reduced isolation box type:   "NoSecurityIsolation".
-                                
-                                // Other features, available with any cert: "UseRamDisk", "ForceUsbDrives",
-                                // as well as Automatic Updates, etc....
+            reservd_4   : 8;
 
-        long expirers_in_sec;
+        unsigned long expirers_in_sec;
     };
 } SCertInfo;
 
 enum ECertType {
-    eCertNoType = eCertEternal = eCertContributor = eCertBusiness = eCertPersonal = 
-    eCertHome = eCertFamily = eCertPatreon = eCertGreatPatreon = 
-    eCertEntryPatreon = eCertEvaluation = 0b00100
-};
-        
-enum ECertLevel {
-    eCertNoLevel = eCertStandard = eCertStandard2 = 
-    eCertAdvanced1 = eCertAdvanced = eCertMaxLevel = 0b111
+    eCertNoType = eCertEternal = eCertContributor = eCertBusiness = eCertPersonal = eCertHome = eCertFamily = eCertPatreon = eCertGreatPatreon = eCertEntryPatreon = eCertEvaluation = 0b00100
 };
 
-#define CERT_IS_TYPE(cert,t)        ((cert.type & 0b11100) == (unsigned long)(t))
+enum ECertLevel {
+    eCertNoLevel = eCertStandard = eCertStandard2 = eCertAdvanced1 = eCertAdvanced = eCertMaxLevel = 0b111
+};
+
+#define CERT_IS_TYPE(cert, t)       ((cert.type == (unsigned long)(t)))
 #define CERT_IS_SUBSCRIPTION(cert)  (CERT_IS_TYPE(cert, eCertBusiness) || CERT_IS_TYPE(cert, eCertHome) || cert.type == eCertEntryPatreon || CERT_IS_TYPE(cert, eCertEvaluation))
-#define CERT_IS_INSIDER(cert)		(CERT_IS_TYPE(cert, eCertEternal) || cert.type == eCertGreatPatreon)
-#define CERT_IS_LEVEL(cert,l)       (cert.active && cert.level >= (unsigned long)(l))
+#define CERT_IS_INSIDER(cert)       (CERT_IS_TYPE(cert, eCertEternal) || cert.type == eCertGreatPatreon)
+#define CERT_IS_LEVEL(cert, l)      (cert.active && cert.level >= (unsigned long)(l))
+
 
 #ifdef KERNEL_MODE
 extern SCertInfo Verify_CertInfo;
