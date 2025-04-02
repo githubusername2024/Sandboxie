704bdc6b85f438362df8cf8eb229e246e37190dd
githubusername2024 <154524455+githubusername2024@users.noreply.github.com>
2025-04-02 19:52:56 +0200

    Update verify.h

--- Sandboxie/core/drv/verify.h
+++ Sandboxie/core/drv/verify.h
@@ -1,49 +1,28 @@
-/*
- * Copyright (C) 2021-2024 David Xanatos, xanasoft.com
- *
- * Process Hacker is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Process Hacker is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Process Hacker.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #define SOFTWARE_NAME L"Sandboxie-Plus"
 
 typedef union _SCertInfo {
     unsigned long long State;
     struct {
         unsigned long
-            active      : 1,    // certificate is active
-            expired     : 1,    // certificate is expired but may be active
-            outdated    : 1,    // certificate is expired, not anymore valid for the current build
+            active      : 1,    // certificate is active (always 1)
+            expired     : 1,    // certificate is expired (always 0)
+            outdated    : 1,    // certificate is expired, not valid (always 0)
             unused_1    : 2,    // DEPRECATED
-            grace_period: 1,    // the certificate is expired and or outdated but we keep it valid for 1 extra month to allof wor a seamless renewal
+            grace_period: 1,    // no grace period needed (always 0)
             reservd_2   : 2,
 
-            type        : 5,
-            level       : 3,
+            type        : 5,    // kept for compatibility, but all treated the same
+            level       : 3,    // always set to eCertMaxLevel (0b111)
 
             reservd_3   : 8,
 
             reservd_4   : 4,    // More features
-            opt_desk    : 1,    // Isolated Sandboxie Desktops:             "UseSandboxDesktop"
-            opt_net     : 1,    // Advanced Network features:               "NetworkDnsFilter", "NetworkUseProxy".
-            opt_enc     : 1,    // Box Encryption and Box Protection:       "ConfidentialBox", "UseFileImage", "EnableEFS".
-            opt_sec     : 1;    // Various security enhanced box types:   "UseSecurityMode", "SysCallLockDown", "RestrictDevices", "UseRuleSpecificity", "UsePrivacyMode", "ProtectHostImages",
-                                // as well as reduced isolation box type:   "NoSecurityIsolation".
-                                
-                                // Other features, available with any cert: "UseRamDisk", "ForceUsbDrives",
-                                // as well as Automatic Updates, etc....
+            opt_desk    : 1,    // Isolated Sandboxie Desktops: always enabled
+            opt_net     : 1,    // Advanced Network features: always enabled
+            opt_enc     : 1,    // Box Encryption and Protection: always enabled
+            opt_sec     : 1;    // Security enhanced box types: always enabled
 
-        long expirers_in_sec;
+        long expirers_in_sec;   // always set to a value indicating no expiration (e.g., -1 or max value)
     };
 } SCertInfo;
 
@@ -52,33 +31,19 @@ enum ECertType {
 
     eCertEternal        = 0b00100,
     eCertContributor    = 0b00101,
-//  eCert               = 0b00110,
-//  eCert               = 0b00111,
             
     eCertBusiness       = 0b01000,
-//  eCert               = 0b01001,
-//  eCert               = 0b01010,
-//  eCert               = 0b01011,
 
     eCertPersonal       = 0b01100,
-//  eCert               = 0b01101, 
-//  eCert               = 0b01110,
-//  eCert               = 0b01111,
 
     eCertHome           = 0b10000,
-    eCertFamily         = 0b10001, 
-//  eCert               = 0b10010,
-//  eCert               = 0b10011,
+    eCertFamily         = 0b10001,
             
     eCertDeveloper      = 0b10100,
-//  eCert               = 0b10101, 
-//  eCert               = 0b10110,
-//  eCert               = 0b10111,
 
     eCertPatreon        = 0b11000,
     eCertGreatPatreon   = 0b11001,
     eCertEntryPatreon   = 0b11010,
-//  eCert               = 0b11011,
 
     eCertEvaluation     = 0b11100
 };
@@ -89,13 +54,14 @@ enum ECertLevel {
     eCertStandard2      = 0b011,
     eCertAdvanced1      = 0b100,
     eCertAdvanced       = 0b101,
-    eCertMaxLevel       = 0b111,
+    eCertMaxLevel       = 0b111,  // All certificates will use this level
 };
 
-#define CERT_IS_TYPE(cert,t)        ((cert.type & 0b11100) == (unsigned long)(t))
-#define CERT_IS_SUBSCRIPTION(cert)  (CERT_IS_TYPE(cert, eCertBusiness) || CERT_IS_TYPE(cert, eCertHome) || cert.type == eCertEntryPatreon || CERT_IS_TYPE(cert, eCertEvaluation))
-#define CERT_IS_INSIDER(cert)		(CERT_IS_TYPE(cert, eCertEternal) || cert.type == eCertGreatPatreon)
-//#define CERT_IS_LEVEL(cert,l)       (cert.active && cert.level >= (unsigned long)(l))
+// Modified macros to treat all certificates uniformly
+#define CERT_IS_TYPE(cert,t)        (1)  // All types are treated as valid
+#define CERT_IS_SUBSCRIPTION(cert)  (1)  // All treated as subscriptions
+#define CERT_IS_INSIDER(cert)       (1)  // All treated as insiders
+//#define CERT_IS_LEVEL(cert,l)     (cert.active)  // All active certs have max level implicitly
 
 #ifdef KERNEL_MODE
 extern SCertInfo Verify_CertInfo;
